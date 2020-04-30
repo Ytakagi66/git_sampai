31f630ab2c58d4d03ec772a0507830af88facd1f
Ytakagi <ptlv66httl@gmail.com>
2020-04-30 13:37:33 +0000

    heroku3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,15 +4,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fit: [500, 500]
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   if Rails.env.production?
     storage :fog
   else
     storage :file
   end
+  
+  # Choose what kind of storage to use for this uploader:
+  # storage :file
+  storage :fog
   
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
